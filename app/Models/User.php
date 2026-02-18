aa7ebb4c02b649b975d620c15ac6169b351646bc
Bikramsinghrana <bikramsinghrajput7262@gmail.com>
2026-02-19 02:10:43 +0530

    Remove  Test case and Rest API

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;  // use any one santum or passport ++++++
-use Laravel\Passport\HasApiTokens;  
 use Spatie\Permission\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
